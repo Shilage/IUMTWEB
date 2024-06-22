0ab078ee932f26789aaa0a2a97a9ade1ce4a0640
Giuseppe Marino <84312623+Shilage@users.noreply.github.com>
2024-06-23 00:30:47 +0200

    Start working on Mongoserver. Appearances files done

---
+++ solution/Mongoserver/controllers/appearancesController.js
@@ -0,0 +1,84 @@
+const { AppearancesModel } = require('../models/appearances_model');
+
+/**
+ * Retrieves all appearances for a specific player by the player's ID.
+ *
+ * @param {string} player_id - The ID of the player.
+ * @returns {Promise<Array>} A promise that resolves with an array of appearance objects.
+ */
+function getAllPlayerAppearances(player_id) {
+    return new Promise((resolve, reject) => {
+        AppearancesModel.find({ "player_id" : player_id})
+            .then((result) =>{
+                resolve(result);
+            })
+            .catch(error=>{
+                console.error("Error in getAllPlayerAppearances:", error);
+                reject(error);
+            });
+    });
+}
+
+
+module.exports.getAllPlayerAppearances = getAllPlayerAppearances;
+/**
+ * Retrieves all appearances for a specific game by the game's ID.
+ *
+ * @param {string} game_id - The ID of the game.
+ * @returns {Promise<Array>} A promise that resolves with an array of appearance objects.
+ */
+function getAllPlayerAppearancesByGameId(game_id) {
+    return new Promise((resolve, reject) => {
+        AppearancesModel.find({ "game_id" : game_id})
+            .then((result) =>{
+                resolve(result);
+            })
+            .catch(error=>{
+                console.error("Error in getAllPlayerAppearancesByGameId:", error);
+                reject(error);
+            });
+    });
+}
+
+
+module.exports.getAllPlayerAppearancesByGameId = getAllPlayerAppearancesByGameId;
+
+/**
+ * Inserts a new document into the database.
+ *
+ * @param {Object} body - The data to be inserted.
+ * @returns {Promise<Object>} A promise that resolves with the result of the insertion operation.
+ */
+function insert(body) {
+    return new Promise((resolve, reject) => {
+        const mongoObj = new Model(body);
+        mongoObj.save()
+            .then(results => {
+                resolve(results);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+}
+
+module.exports.insert = insert;
+
+/**
+ * Retrieves all documents from the database.
+ *
+ * @returns {Promise<Array>} A promise that resolves with an array of documents.
+ */
+function query() {
+    return new Promise((resolve, reject) => {
+        Model.find()
+            .then((result) =>{
+                resolve.json(result);
+            })
+            .catch(error=>{
+                reject(error);
+            });
+    });
+}
+
+module.exports.query = query;
---
+++ solution/Mongoserver/routes/appearancesRoute.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+const path = require('path');
+router.use( bodyParser.json() );
+const appearances_controller = require('../controllers/appearancesController.js')
+
+/**
+ * Route to get player appearances by player ID.
+ *
+ * @route GET /get-player-appearances-by-player-id/:player_id
+ * @param {string} player_id - The ID of the player.
+ * @returns {Object[]} players - Returns a JSON array of player appearances.
+ * @returns {Object} error - Returns an error object in case of failure.
+ */
+router.get('/get-player-appearances-by-player-id/:player_id', async function(req, res, next) {
+    const player_id = req.params.player_id;
+    try {
+        const players = await appearances_controller.getAllPlayerAppearances(player_id);
+        res.json(players);
+    } catch (error) {
+        console.error('Error retrieving players appearances:', error.message);
+        res.status(500).json({ error: 'Internal Server Error appearances' });
+    }
+});
+
+/**
+ * Route to get player appearances by game ID.
+ *
+ * @route GET /get-player-appearances-by-game-id/:game_id
+ * @param {string} game_id - The ID of the game.
+ * @returns {Object[]} games - Returns a JSON array of player appearances for the specified game.
+ * @returns {Object} error - Returns an error object in case of failure.
+ */
+router.get('/get-player-appearances-by-game-id/:game_id', async function(req, res, next) {
+    const game_id = req.params.game_id;
+    try {
+        const games = await appearances_controller.getAllPlayerAppearancesByGameId(game_id);
+        res.json(games);
+    } catch (error) {
+        console.error('Error retrieving players appearances: BY GAME D', error.message);
+        res.status(500).json({ error: 'Internal Server Error appearances BY GAME ID' });
+    }
+});
+
+module.exports = router;
